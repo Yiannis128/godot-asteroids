e413a6b54aa42dfc6f6580648ebf0a2a07376570
Yiannis Charalambous <yiannis128@hotmail.com>
2021-12-07 22:02:41 +0000

    Finished part 3

--- code/Asteroid.gd
+++ code/Asteroid.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
+var move_dir : Vector2 = Vector2.ZERO
+
 func _ready() -> void:
     connect("area_entered", area_entered)
 
 func area_entered(area : Area2D) -> void:
     queue_free()
 
+func _physics_process(delta: float) -> void:
+    position += move_dir * delta
